9512c718e52cf710378dfab8a188d8f225b301c7
Jmgarcia30 <jmjgarcia.student@ua.edu.ph>
2025-11-04 09:10:30 +0800

    Added delete

--- pages/UserListPage.js
+++ pages/UserListPage.js
@@ -1,4 +1,4 @@
-import {View, Text, Button, ActivityIndicator, FlatList} from 'react-native';
+import {View, Text, Button, ActivityIndicator, FlatList, Alert} from 'react-native';
 import axios from 'axios';
 import { useState } from 'react';
 import { useEffect } from 'react/cjs/react.development';
@@ -29,6 +29,33 @@ export default function UserListPage(){
         );
     }
 
+    const handleDelete = (id) => {
+        Alert.alert(
+            "Confirm Delete",
+            "Are you sure you want to Delete this user?",
+            [
+                {text: "Cancel", style: "cancel"},
+                {
+                    text: "Delete",
+                    style: "destructive",
+                    onPress: () => {
+                        axios
+                        .delete(`http://192.168.30.109:8000/registration/api/users/${id}/`)
+                        .then(() => {
+                            Alert.alert("Success", "User deleted successfully");
+                        })
+                        .catch((err)=>{
+                            console.error(err);
+                            Alert.alert("Error", "Failed to delete user");
+                        });
+                    },
+                },
+            ]
+        );  
+    };
+    
+    
+
     return(
         <View>
             <Text style={styles.title}>User List Page</Text>
@@ -43,11 +70,14 @@ export default function UserListPage(){
 
                         <Button
                         title="Edit"
-                        color="#40d32dff"/>
+                        color="#40d32dff"
+                        />
 
                         <Button
                         title="Delete"
-                        color="#ff3b30"/>
+                        color="#ff3b30"
+                        onPress={()=> handleDelete(item.id)}
+                        />
                     </View>
                 )}
             />
